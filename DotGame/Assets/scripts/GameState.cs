5af541840a1ff65f6d23874df993f6859a420f3a
Ivan Franco <ifrancoh@ucsc.edu>
2023-11-15 23:04:21 -0800

    moved toycar image inside toycar proj, added 2D game (GotGame) for 3D remake attempt

---
+++ DotGame/Assets/scripts/GameState.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    public static int state;
+    public static int level = 1;
+    public const int gamePlay = 1;
+    public const int gameOver = 2;
+    public const int levelComplete = 3;
+    public const int theEnd = 4;
+
+    public const int MenuScene = 1;
+    public const int FirstLevelScene = 2;
+    public const int SceneOffset = FirstLevelScene - 1;
+
+    // Start is called before the first frame update
+
+ //   private void Awake()
+ //   {
+ //      DontDestroyOnLoad(gameObject);
+ //   }
+
+
+    void Start()
+    {
+        state = gamePlay;
+    }
+
+}
